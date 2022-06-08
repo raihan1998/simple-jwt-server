2c71299899e5a0a37df7d94325ec44e15bfa8ef7
unknown <you@example.com>
2022-06-08 13:59:38 +0600

    jwt add

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const express =require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const res = require("express/lib/response");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -9,10 +11,44 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello simple form jwt.....');
-})
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  console.log('inside verify token', authHeader);
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello simple form jwt.....");
+});
+
+app.post("/login", (req, res) => {
+  const user = req.body;
+  console.log(user);
+  // Dangerous: Do not check password here for serious application
+  // Use proper process for hashing and checking
+  // After completing all authentication related verification, issue JWT token
+  // not real time used : user.email === 'user@gmail.com'
+  if (user.email === "user@gmail.com" && user.password === "12345") {
+    const accessToken = jwt.sign(
+      { email: user.email },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "2h" }
+    );
+    res.send({
+      success: true,
+      accessToken: accessToken,
+    });
+  } else {
+    res.send({ success: false });
+  }
+});
+
+app.get("/orders", verifyJWT, (req, res) => {
+  res.send([
+    { id: 1, item: "apple" },
+    { id: 2, item: "oranges" },
+  ]);
+});
 
 app.listen(port, () => {
-    console.log('Listening to port', port)
-})
+  console.log("Listening to port", port);
+});
